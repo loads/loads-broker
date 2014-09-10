c1c0969e8c920c1af5ae03691533c44365946197
Tarek Ziade <tarek@ziade.org>
2014-09-10 15:04:46 +0200

    plugged a /run/run_id api

--- loadsbroker/tests/test_client.py
+++ loadsbroker/tests/test_client.py
@@ -3,50 +3,69 @@ import sys
 import unittest
 import requests
 import time
-from contextlib import contextmanager
 
 from loadsbroker.client import Client
 from loadsbroker import __version__
 
 
-@contextmanager
-def broker():
-    cmd = 'from loadsbroker.main import main; main()'
-    cmd = '%s -c "%s"' % (sys.executable, cmd)
-    broker = subprocess.Popen(cmd, shell=True,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-
-    # wait for the broker to be ready
-    starting = time.time()
-    started = False
-    while time.time() - starting < 1:
-        try:
-            requests.get('http://127.0.0.1:8080', timeout=.1)
-            started = True
-            break
-        except Exception as e:
-            time.sleep(.1)
-
-    if not started:
-        print('Could not start the broker!')
-        broker.kill()
-        raise e
-
-    yield
-
-    try:
-        broker.terminate()
-    finally:
-        broker.kill()
-
-
 class TestClient(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cmd = 'from loadsbroker.main import main; main()'
+        cmd = '%s -c "%s"' % (sys.executable, cmd)
+        broker = subprocess.Popen(cmd, shell=True,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+
+        # wait for the broker to be ready
+        starting = time.time()
+        started = False
+
+        errors = []
+
+        while time.time() - starting < 1:
+            try:
+                requests.get('http://127.0.0.1:8080', timeout=.1)
+                started = True
+                break
+            except Exception as exc:
+                errors.append(exc)
+                time.sleep(.1)
+
+        if not started:
+            print('Could not start the broker!')
+            broker.kill()
+            if len(errors) > 0:
+                raise errors[-1]
+            else:
+                raise Exception()
+
+        cls.broker = broker
+        cls.client = Client()
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.broker.terminate()
+        finally:
+            cls.broker.kill()
+
     def test_info(self):
-
-        with broker():
-            c = Client()
-            res = c('info')
-
+        res = self.client('info')
         self.assertEqual(res['version'], __version__)
+
+    def test_launch_run(self):
+        res = self.client('run')
+        self.assertTrue('run_id' in res)
+
+        run_id = res['run_id']
+
+        # checking the run exists
+        res = self.client('status', run_id)
+
+        # aborting the run
+        res = self.client('abort', run_id)
+
+        # checking the run is not listed anymore
+        res = self.client('status', run_id)
