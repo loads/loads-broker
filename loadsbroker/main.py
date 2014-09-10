e2d6bc7c0589f602777cf7b3ab98081d6d8b3ab1
Tarek Ziade <tarek@ziade.org>
2014-09-10 09:56:08 +0200

    started a client script to drive the broker

--- loadsbroker/main.py
+++ loadsbroker/main.py
@@ -1,33 +1,11 @@
 import sys
 import argparse
-import logging
-import logging.handlers
-
 import tornado.ioloop
 
+from loadsbroker.util import set_logger
 from loadsbroker.broker import Broker
 from loadsbroker.api import application
 from loadsbroker import logger
-
-
-def _set_logger(debug=False, name='loads', logfile='stdout'):
-    logger_ = logging.getLogger(name)
-    logger_.setLevel(logging.DEBUG)
-    logger.propagate = False
-
-    if logfile == 'stdout':
-        ch = logging.StreamHandler()
-    else:
-        ch = logging.handlers.RotatingFileHandler(logfile, mode='a+')
-
-    if debug:
-        ch.setLevel(logging.DEBUG)
-    else:
-        ch.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('[%(asctime)s][%(process)d] %(message)s')
-    ch.setFormatter(formatter)
-    logger_.addHandler(ch)
 
 
 def _parse(sysargs=None):
@@ -52,7 +30,7 @@ def _parse(sysargs=None):
 
 def main(sysargs=None):
     args, parser = _parse(sysargs)
-    _set_logger(debug=args.debug)
+    set_logger(debug=args.debug)
     loop = tornado.ioloop.IOLoop.instance()
     application.broker = Broker(loop, args.database, args.ssh_key,
                                 args.ssh_username)
