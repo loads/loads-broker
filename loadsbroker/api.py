53c849a520ccb449735c247957e400392e82d688
Tarek Ziade <tarek@ziade.org>
2014-09-11 11:30:36 +0200

    move basic json logic into a base handler class

--- loadsbroker/api.py
+++ loadsbroker/api.py
@@ -1,36 +1,83 @@
 #
 # HTTP APIs
 #
+import json
 import os
+
 import tornado.web
 from loadsbroker import __version__
 
 
-# CoreOS-stable-367.1.1
-COREOS_IMG = 'ami-3193e801'
-USER_DATA = os.path.join(os.path.dirname(__file__), 'aws.yml')
+_DEFAULTS = {'ami': 'ami-3193e801',
+             'nodes': 1,
+             'user_data': os.path.join(os.path.dirname(__file__), 'aws.yml')}
 
 
-class RootHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, **kw):
+        super(BaseHandler, self).__init__(application, request, **kw)
+        self.broker = application.broker
+        self.db = self.broker.db
+
+    def set_default_headers(self):
+        self.set_header('Content-Type', 'application/json')
+
+    def write_error(self, status=400, **kw):
+        self.set_status(status)
+        if 'message' not in kw:
+            if status == 405:
+                kw['message'] = 'Invalid HTTP method.'
+            else:
+                kw['message'] = 'Unknown error.'
+
+        self.response = kw
+        self.write_json()
+
+    def prepare(self):
+        if self.request.body:
+            body = self.request.body.decode('utf8')
+            try:
+                json_data = json.loads(body)
+                self.request.arguments.update(json_data)
+            except ValueError as e:
+                self.send_error(400, message=str(e))
+
+        self.response = {}
+
+    def write_json(self):
+        output = json.dumps(self.response)
+        self.write(output)
+
+
+class RootHandler(BaseHandler):
     def get(self):
-        self.write({'version': __version__})
+        self.response['version'] = __version__
+        self.write_json()
 
     def post(self):
+        # need more sanitizing here
+        options = dict(self.request.arguments)
+
+        for key, val in _DEFAULTS.items():
+            if key not in options:
+                options[key] = val
+
         # run a new test
-        uuid = self.application.broker.run_test(ami=COREOS_IMG,
-                                                user_data=USER_DATA,
-                                                nodes=1)
-        self.write({'run_id': uuid})
+        options['run_id'] = self.broker.run_test(**options)
+        self.response = options
+        self.write_json()
 
 
 # TODO / db queries
-class RunHandler(tornado.web.RequestHandler):
+class RunHandler(BaseHandler):
 
     def delete(self, run_id):
-        self.write({'result': 'OK'})
+        self.response['result'] = 'OK'
+        self.write_json()
 
     def get(self, run_id):
-        self.write({'status': 'OK'})
+        self.response['result'] = 'OK'
+        self.write_json()
 
 
 application = tornado.web.Application([
