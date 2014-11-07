40f6f6a158b630fa1a55308dd75a993178f98186
Ben Bangert <ben@groovie.org>
2014-11-07 14:29:45 -0800

    Refactor to load the db from a JSON file. Include a default pushgo project JSON file. Refactor trigger API, add abort API.

--- loadsbroker/webapp/__init__.py
+++ loadsbroker/webapp/__init__.py
@@ -1,6 +1,10 @@
 import os
 import tornado.web
-from loadsbroker.webapp.api import RootHandler, RunHandler
+from loadsbroker.webapp.api import (
+    RootHandler,
+    RunHandler,
+    OrchestrateHandler
+)
 from loadsbroker.webapp.views import GrafanaHandler
 
 
@@ -10,5 +14,6 @@ _GRAFANA = os.path.join(os.path.dirname(__file__), 'grafana')
 application = tornado.web.Application([
     (r"/api", RootHandler),
     (r"/api/run/(.*)", RunHandler),
-    (r"/dashboards/run/([^\/]*)/(.*)", GrafanaHandler, {"path": _GRAFANA, "default_filename": "index.html"})
+    (r"/api/orchestrate/(.*)", OrchestrateHandler),
+    (r"/dashboards/run/([^\/]+)/(.*)", GrafanaHandler, {"path": _GRAFANA, "default_filename": "index.html"})
 ])
